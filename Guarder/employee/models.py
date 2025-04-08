9c61e411251d0b29a6dd9d5dd6e302d06a535821
Nisha Singh <nisha311035@gmail.com>
2025-04-08 19:01:15 +0530

    Internal Web v1

--- Guarder/employee/models.py
+++ Guarder/employee/models.py
@@ -2,8 +2,9 @@ from django.db import models
 import qrcode
 from io import BytesIO
 from django.core.files.base import ContentFile
+import hashlib
 class Employee(models.Model):
-    employee_id = models.CharField(max_length=10, primary_key=True)  # Use employee ID as primary key
+    employee_id = models.CharField(max_length=10, primary_key=True, blank=True)  # Use employee ID as primary key
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)  # Store hashed passwords
     # Add other relevant fields
@@ -17,6 +18,18 @@ class Employee(models.Model):
         return f"{self.name} ({self.employee_id})"  # Display name and ID in admin
 
     def save(self, *args, **kwargs):
+
+        if not self.employee_id:
+            combined_data = f"{self.name}{self.email}{self.dob}{self.mobile_number}{self.aadhar_number}" # Use first 8 characters of hash
+            counter = 0
+            while True:
+                hashed_data = hashlib.sha256(f"{combined_data}{counter}".encode()).hexdigest()
+                numeric_hash = int(hashed_data, 16) % 900000 + 100000  # Ensure it's between 100000 and 999999
+                if not Employee.objects.filter(employee_id=numeric_hash).exists():
+                    self.employee_id = numeric_hash
+                    break
+                counter += 1
+
         # Generate a URL that redirects to the employee's details page
         qr_data = f"https://yourdomain.com/details/{self.employee_id}/"
 
